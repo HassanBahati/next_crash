b97622c732c8aa1b7b9d04d6c2c03d38fa3e7307
HassanBahati <mukisabahati@gmail.com>
2021-08-16 21:27:59 +0300

    created about page

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,12 @@ import Head from 'next/head'
 export default function Home() {
   return (
   <div>
+    <Head>
+      <title>Next Crash</title>
+      <meta name="keyword"/>
+    </Head>
     <h1>
-      Welcome to NExt 
+      Welcome to Next 
     </h1>
   </div>
     )
